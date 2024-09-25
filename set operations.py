71e37a1df2180d4f6a2e88e59df722bb3f5f886c
Zabir Hasan <dominicancongo@gmail.com>
2024-09-25 21:26:05 +0600

    first commit

---
+++ set operations.py
@@ -0,0 +1,11 @@
+mySet = {2, 1, 3, "Zabir", "Zabir", "Tabid"}
+print(mySet)
+
+mySet.add("Diddys freakoffs")
+print(mySet)
+mySet.pop()
+print(mySet)
+
+lst = [5,6,7]
+setList = set(lst)
+print(setList, type(setList))
